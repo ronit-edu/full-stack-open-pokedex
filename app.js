533eb2a7fd2727fe310e8d862637b5b949b22346
Roni Tarvainen <roni.tarvainen@gmail.com>
2024-05-02 19:52:30 +0300

    made health check scheduled

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.status(404).end()
-  //////res.send('guthib.com')
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
