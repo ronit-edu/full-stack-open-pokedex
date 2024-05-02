b41d4869d28469073aeee1317401a542ec1b29b4
Roni Tarvainen <roni.tarvainen@gmail.com>
2024-05-02 19:20:29 +0300

    changed poke api url to invalid

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
 
 const App = () => {
   const match = useMatch('/pokemon/:name')
-  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
+  const { data: pokemonList, error, isLoading } = useApi('https://pkeapi.co/api/v2/pokemon/?limit=50', mapResults)
 
   if (isLoading) {
     return <LoadingSpinner />
